295c648b770a8ea9c2e9c2e9b999b456ac2b2d66
golgetozu <golgetozu@gmail.com>
2026-07-15 20:25:56 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,55 @@
+const CACHE_NAME = "nexus-trace-v1";
+const ASSETS_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  // Yalnızca GET isteklerini önbelleğe al, API/Firebase isteklerini atla
+  if (event.request.method !== 'GET' || event.request.url.includes('firestore') || event.request.url.includes('firebase')) {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request).then((fetchResponse) => {
+        return caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, fetchResponse.clone());
+          return fetchResponse;
+        });
+      });
+    }).catch(() => {
+      // Çevrimdışıysak ve index.html isteniyorsa (veya kök dizin)
+      if (event.request.mode === 'navigate') {
+        return caches.match('./index.html');
+      }
+    })
+  );
+});
